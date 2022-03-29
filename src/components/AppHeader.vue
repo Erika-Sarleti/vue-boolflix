d1fd91b81e315b3c93b00234f2fd20ef05e0056e
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-29 12:02:51 +0200

    struttura

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <label for="searchbar"></label>
+      <input type="text" name="searchbar" v-model="userInput">
+      <button @click="$emit('search', userInput )" >Cerca</button>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+
+    data(){
+        return{
+            userInput:'',
+        } 
+    }
+}
+</script>
+
+<style>
+
+</style>
