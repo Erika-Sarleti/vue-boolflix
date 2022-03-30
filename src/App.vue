6c34bbe8378310440e645968ea42664b021c72e2
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-30 12:02:04 +0200

    errore

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <AppHeader/>
+    <AppHeader @search="findMovie"/>
     <AppMain/>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue'
+import AppMain from './components/AppMain.vue';
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
    AppHeader,
    AppMain
+  },
+  data(){
+    return{
+      movieArr:[],
+      apiKey: 'ba3c7b44f63406966919314d1d6a1d9d',
+    }
+  },
+  methods:{
+    findMovie(userInput){
+      axios.get('https://api.themoviedb.org/3/movie/', {
+    params: {
+      api_key: this.apiKey,
+      language: 'it-IT',
+      query: userInput,
+    }
+  }).then(response => {
+    this.movieArr = response.data
+  })
+    }
   }
 }
 </script>
