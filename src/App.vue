377fcc602f6507b3e2b34c2d6356e4f8a0fd4c44
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-30 16:44:02 +0200

    Errore sull'app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <AppHeader @search="findSearch"/>
-    <AppMain  :/>
+    <AppMain :arr-movies="movieArr" :arr-series="arrSeries"/>
   </div>
 </template>
 
@@ -19,6 +19,7 @@ export default {
   data(){
     return{
       movieArr:[],
+      arrSeries:[],
       apiKey: 'ba3c7b44f63406966919314d1d6a1d9d',
     }
   },
@@ -30,43 +31,35 @@ export default {
             language: 'it-IT',
             query: userInput,
       }
-
       this.apiCall('movie', objParams)
-      this.apiCall('tv', objParams)
-      
-
-        
+      this.apiCall('tv', objParams)  
       } else{
         this.movieArr= [];
+        this.arrSeries
       }
     },
-    apiCall(searchType, objParams){
-    axios.get(`https://api.themoviedb.org/3/search/ ${searchType}`,{
-      params: objParams,
-    })
-      .then(response => {
-        if (searchType == 'movie'){
-          this.movieArr = response.data.results.map((movie) =>
-           ({
+  apiCall(searchType, objParams){
+    axios.get(`https://api.themoviedb.org/3/search/${searchType}`,{params: objParams,})
+    .then(response =>{
+      if (searchType === 'movie') {
+        this.movieArr = response.data.results.map((movie) => ({
               id: movie.id,
               title: movie.title,
               originalTitle: movie.original_title,
               language: movie.original_language,
               rating: movie.vote_average,
-            })
-          )
-        } else {
-            this.arrSeries = response.data.results.map((series) => ({
-              id: series.id,
-              title: series.name,
-              originalTitle: series.original_name,
-              language: series.original_language,
-              rating: series.vote_average,
             }));
-          }
-        }
+    } else {
+        this.arrSeries = response.data.results.map(series =>({
+            id: series.id,
+          title: series.name,
+          originalTitle: series.original_name,
+          language: series.original_language,
+          rating: series.vote_average,
+        }))
       }
-    }
+    })
+  },
   }
 </script>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,24 @@
 <template>
   <main>
-
+    <BooflixCard v-for="movie in arrMovies"
+    :key='movie.id'
+    :info='movie'
+    />
   </main>
 </template>
 
 <script>
+import BooflixCard from './components/BooflixCard.vue';
+
 export default {
     name: 'AppMain',
+    components:{
+      BooflixCard,
+    },
+    props:{
+      arrMovies: Array,
+      arrSeries: Array,
+    },
 }
 </script>
 
